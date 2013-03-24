bdec3e08b1ee598b5a029d9d94407eb9feeaa1a5
manutero <manutero@gmail.com>
2013-03-24 16:48:51 +0100

    init project on XCode

---
+++ iOs_touch-and-move/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOs_touch-and-move
+//
+//  Created by Manuel Artero Anguita on 24/03/13.
+//  Copyright (c) 2013 Manuel Artero Anguita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
